24c943a2d28035f5938813ea88496116859a2e89
codeRazX <razapCode@gmail.com>
2025-05-08 14:59:30 +0000

    finish project

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res)=>{
+  try{
+    const file = req.file;
+    if(!file) throw Error;
+    const {mimetype, size, originalname} = file;
+    res.json({ name:originalname, type: mimetype, size: size,});
+  }
+  catch(err){
+    
+    res.send(err);
+  }
+  
+
+
+})
 
 
 
